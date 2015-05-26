445bdd5986e9e0e7de448385dab1c288198a341e
cocolote <skiel.j.lopez@gmail.com>
2015-05-26 18:22:16 -0400

    fixing some details

--- caller.py
+++ caller.py
@@ -9,37 +9,39 @@ import praw
 
 app = Flask(__name__)
 
-
 @app.route('/')
 def home():
-  return render_template('home.html')
+    return render_template('home.html')
 
 @app.route('/make_call')
 def make_call():
-  account_sid = os.environ['ACCOUNT_SID']
-  auth_token = os.environ['AUTH_TOKEN']
-  client = TwilioRestClient(account_sid, auth_token)
+    account_sid = os.environ['ACCOUNT_SID']
+    auth_token = os.environ['AUTH_TOKEN']
+    client = TwilioRestClient(account_sid, auth_token)
 
-  call = client.calls.create(to="+13129523553",
-                             from_="+16175130992",
-                             url="http://obscure-dawn-3571.herokuapp.com/message")
+    call = client.calls.create(to="+13054406756",
+                               from_="+16175130992",
+                               url="http://obscure-dawn-3571.herokuapp.com/message")
 
-  return "Call in progress"
+    return "Call in progress"
 
 @app.route('/message', methods=['GET','POST'])
 def message():
-  reload(sys)
-  sys.setdefaultencoding('utf8')
-  r = praw.Reddit(user_agent='web_caller_zeke')
-  titles = r.get_subreddit('technology').get_top(limit=1)
-  
-  resp = twilio.twiml.Response()
-  resp.say("Hey Chris!. How is it going?. This is Ezequiel with some news from Reddit.")
-  for x in titles:
-    resp.say(str(x))
-  resp.say("Cheers!")
+    reload(sys)
+    sys.setdefaultencoding('utf8')
+    r = praw.Reddit(user_agent='web_caller_zeke')
+    titles = r.get_front_page(limit=1)
+    
+    resp = twilio.twiml.Response()
+    resp.say("Hey Chris!. How is it going?.\
+    This is Ezequiel with some news from Reddit.")
+    
+    for x in titles:
+      resp.say(str(x.title))
+    
+    resp.say("Cheers!")
 
-  return Response(str(resp), mimetype='text/xml')
+    return Response(str(resp), mimetype='text/xml')
 
 # if __name__ == "__main__":
 #   app.run(debug=True)
--- static/myjs.js
+++ static/myjs.js
@@ -1,19 +1,22 @@
 $(function(){
   var myVar=setInterval(function(){myTimer()},1000);
 
-  var myTime = new Date("May 26, 2015 16:00:00");
+  var myTime = new Date("May 26, 2015 18:25:00");
   function myTimer() {
     var d = new Date();
     var dd = Date(myTime - d);
     var h = Math.floor(((myTime - d)/1000) / 3600);
     var m = Math.floor(((myTime - d)/(1000 * 60)) % 60);
     var s = Math.floor(((myTime - d)/1000) % 60);
+
     $("#clock").text(h+':'+m+':'+s);
+
     if (h <= 0 && m <= 0 && s <= 0){
       clearInterval(myVar);
       $('#clock').empty();
       $.get('/make_call');
     }
+
   }
 });
 
